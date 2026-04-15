dfe5550140832578db5350a4beff54df3a3f6bb0
Rune Morling <ermo@aerynos.com>
2026-04-15 15:51:21 +0200

    moss/cli: Convert extract to use clap derive structs
    Signed-off-by: Rune Morling <ermo@aerynos.com>

--- moss/src/cli/extract.rs
+++ moss/src/cli/extract.rs
@@ -3,9 +3,34 @@
 
 use std::path::PathBuf;
 
-use clap::{ArgMatches, Command, arg};
+use clap::Parser;
 
 pub use moss::client::extract::Error;
+
+#[derive(Debug, Parser)]
+#[command(
+    about = "Extract contents of a moss-format archive to disk",
+    long_about = "For all valid moss-format content-bearning archives, extract to disk"
+)]
+pub struct Command {
+    #[arg(help = "valid moss-format archives(s) to extract")]
+    files: Vec<PathBuf>,
+    #[arg(
+        long = "output-dir",
+        help = "directory to which to extract moss-format archive(s) (default: `.`)",
+        default_value = "."
+    )]
+    output_dir: Option<PathBuf>,
+}
+
+pub fn handle(command: Command) -> Result<(), Error> {
+    let paths = command.files.into_iter().flatten().collect::<Vec<_>>();
+    let output_dir = command.output_dir.unwrap_or_default();
+
+    moss::client::extract(paths, output_dir)?;
+
+    Ok(())
+}
 
 pub fn command() -> Command {
     Command::new("extract")
@@ -20,7 +45,7 @@ pub fn command() -> Command {
 }
 
 /// Handle the `extract` command
-pub fn handle(args: &ArgMatches) -> Result<(), Error> {
+pub fn handle_old(args: &ArgMatches) -> Result<(), Error> {
     let paths = args
         .get_many::<PathBuf>("PATH")
         .into_iter()
