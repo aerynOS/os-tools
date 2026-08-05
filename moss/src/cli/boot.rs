6b03479cd0e06b5866c5204652e81f6295875036
Fabio Forni <development@redaril.me>
2026-08-05 12:54:12 +0200

    moss: cli: boot: Move handle() into impl Command

--- moss/src/cli/boot.rs
+++ moss/src/cli/boot.rs
@@ -2,42 +2,41 @@
 // SPDX-License-Identifier: MPL-2.0
 
 use clap::Parser;
-use thiserror::Error;
-
 use moss::{Client, Installation, client, environment};
 
 #[derive(Debug, Parser)]
 #[command(about = "Manage boot configuration via blsforme")]
 pub struct Command {
     #[command(subcommand)]
-    subcommand: Subcommand, // NOTE: No Option, because a subcommand is required
+    subcommand: Subcommand,
+}
+
+impl Command {
+    pub fn handle(self, installation: Installation) -> Result<(), client::Error> {
+        match self.subcommand {
+            Subcommand::Status => status(installation),
+            Subcommand::Sync => sync(installation),
+        }
+    }
 }
 
 #[derive(Debug, clap::Subcommand)]
-pub enum Subcommand {
+enum Subcommand {
     #[command(about = "Show boot configuration status")]
     Status,
     #[command(about = "Synchronize boot configuration")]
     Sync,
 }
 
-/// Handle status for now
-pub fn handle(command: Command, installation: Installation) -> Result<(), Error> {
-    match command.subcommand {
-        Subcommand::Status => status(installation),
-        Subcommand::Sync => sync(installation),
-    }
-}
-
-fn status(installation: Installation) -> Result<(), Error> {
-    let client = Client::new(environment::NAME, installation).map_err(Error::Client)?;
+fn status(installation: Installation) -> Result<(), client::Error> {
+    let client = Client::new(environment::NAME, installation)?;
 
     client.print_boot_status()?;
 
     Ok(())
 }
 
-fn sync(installation: Installation) -> Result<(), Error> {
+fn sync(installation: Installation) -> Result<(), client::Error> {
     let client = Client::new(environment::NAME, installation)?;
 
     client.synchronize_boot()?;
@@ -47,10 +46,4 @@ fn sync(installation: Installation) -> Result<(), Error> {
     client.print_boot_status()?;
 
     Ok(())
-}
-
-#[derive(Debug, Error)]
-pub enum Error {
-    #[error("client")]
-    Client(#[from] client::Error),
 }
