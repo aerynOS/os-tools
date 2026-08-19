07b3c1078726b9641a454999132c5962c3fe7c5d
Fabio Forni <development@redaril.me>
2026-08-19 15:26:57 +0200

    crates: fnmatch: Avoid regex

--- crates/fnmatch/src/lib.rs
+++ crates/fnmatch/src/lib.rs
@@ -1,285 +1,73 @@
 // SPDX-FileCopyrightText: 2023 AerynOS Developers
 // SPDX-License-Identifier: MPL-2.0
 
-//! Use fnmatch to generate regex matchers from glob-style strings.
+//! Implementation of an [`fnmatch`](https://man7.org/linux/man-pages/man3/fnmatch.3.html)-like
+//! path matcher.
 //!
-//! This crate extends the conventional `glob` style strings to add matching groups
-//! by compiling to an internal [Regex].
+//! It can match combinations of literal strings, the `*` (pronounced "star") wildcard
+//! that matches zero or more characters, and the `?` wildcard that matches exactly one
+//! character. The matcher is UTF-8-compatible, so here a character is really a
+//! [Unicode scalar value](https://www.unicode.org/glossary/#unicode_scalar_value).
+//! This module works on file paths, so the path separator is special and won't be matched
+//! by wildcards.
 //!
+//! Additionally, the matcher allows named wildcards, that similarly to regex's capture
+//! groups, associate a name to the substring the wildcard resolved into. Named groups
+//! are represented by the following syntax: `(groupname:wildcard)`, where `groupname` is
+//! a user-specified name and `wildcard` is one of the supported wildcards.
 //!
-//! # Example
+//! The matching algorithm is the [*Sea of Stars*](https://git.musl-libc.org/cgit/musl/tree/src/regex/fnmatch.c).
+//!
+//! # Examples
 //! ```
-//!     let pattern = "/usr/lib/modules/(version:*)/kernel".parse::<fnmatch::Pattern>().unwrap();
-//!     let result = pattern.match_path("/usr/lib/modules/6.2.3/kernel").expect("no kernel match");
+//!use fnmatch::Pattern;
+//!
+//!assert!(
+//!    Pattern::new("/usr/bin/mkfs.*".to_string())
+//!        .matches("/usr/bin/mkfs.ext4")
+//!        .is_some()
+//!);
+//!
+//! let matches = Pattern::new("/usr/bin/mkfs.(filesystem:*)".to_string()).matches("/usr/bin/mkfs.ext4");
+//! assert!(matches.is_some_and(|m| m["filesystem"] == "ext4"));
 //! ```
 
-use std::collections::{BTreeMap, BTreeSet};
-use std::str::FromStr;
+use std::{collections::HashMap, ops::Range, path::MAIN_SEPARATOR};
 
-use regex::Regex;
-use serde_core::{Deserialize, de};
-use snafu::{ResultExt as _, Snafu};
+use serde_core::de;
 
-#[derive(Debug)]
-enum Fragment {
-    /// `?`
-    MatchOne,
+use crate::token::{Matcher, Token, tokens};
 
-    /// `*`
-    MatchAnyExceptForwardSlash,
+mod token;
 
-    /// `**`
-    MatchAny,
-
-    /// `\`
-    BackSlash,
-
-    /// `.`
-    Dot,
-
-    /// `/
-    ForwardSlash,
-
-    /// Normal text.
-    Text(String),
-
-    /// Group: Name to fragment mapping
-    Group(String, Vec<Fragment>),
-}
-
-#[derive(Clone)]
-struct StringWalker<'a> {
-    data: &'a str,
-    index: usize,
-    length: usize,
-}
-
-impl Iterator for StringWalker<'_> {
-    type Item = char;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        if self.index >= self.length {
-            None
-        } else {
-            self.index += 1;
-            self.data.get(self.index - 1..self.index).and_then(|s| s.chars().nth(0))
-        }
-    }
-}
-
-impl<'a> StringWalker<'a> {
-    /// Return a new StringWalker
-    pub fn new(data: &'a str) -> Self {
-        Self {
-            data,
-            index: 0,
-            length: data.len(),
-        }
-    }
-
-    pub fn eat(&mut self, much: usize) {
-        self.index += much;
-    }
-
-    pub fn peek(&self) -> Option<char> {
-        self.data.get(self.index..self.index + 1).and_then(|s| s.chars().nth(0))
-    }
-
-    /// Find next occurrence of the character, and substring up to it
-    pub fn substring_to(&self, c: char) -> Option<&'a str> {
-        // Clone ourselves and search that iterator.
-        let walker = self.clone();
-        for (idx, t) in walker.enumerate() {
-            if t == c {
-                return self.data.get(self.index..self.index + idx);
-            }
-        }
-        None
-    }
-}
-
-/// Glob-style matching with groups
-///
-/// You can generate a Pattern by converting from a [String] or string-type
-/// using the [FromStr] trait.
-#[derive(Debug, Clone)]
+/// The path matcher. See [crate-level](crate)
+/// documentation for usage.
+#[derive(Clone, Debug)]
 pub struct Pattern {
     pattern: String,
-    regex: Regex,
-    groups: Vec<String>,
+    tokens: Vec<Token>,
 }
 
-/// Path match for a [Pattern]
-///
-/// A Match contains the matching path as well as any captured
-/// variables, as determined by the [Pattern]
-///
-/// Generate a Match by calling [`Pattern::match_path()`]
-#[derive(Debug)]
-pub struct Match {
-    pub path: String,
-
-    /// Captured variables, as defined by the [`Pattern::groups()`]
-    pub variables: BTreeMap<String, String>,
-}
-
-impl Pattern {
-    /// Attempt to match `path` to our `pattern`
-    ///
-    /// Returns a [Match] if the input path matches the pattern
-    pub fn match_path(&self, path: &str) -> Option<Match> {
-        match self.regex.captures(path) {
-            Some(m) => {
-                let kv = self
-                    .groups
-                    .iter()
-                    .map(|k| (k.clone(), m.name(k).unwrap().as_str().to_owned()));
-                Some(Match {
-                    path: path.into(),
-                    variables: kv.collect(),
-                })
-            }
-            None => None,
-        }
-    }
-
-    /// Return a copy of the internal capture groups
-    pub fn groups(&self) -> Vec<String> {
-        self.groups.clone()
+impl<S: Into<String>> From<S> for Pattern {
+    fn from(value: S) -> Self {
+        let pattern = value.into();
+        let tokens = tokens(&pattern);
+        Self { pattern, tokens }
     }
 }
 
-#[derive(Debug, Snafu)]
-pub enum Error {
-    /// Illegal group syntax
-    #[snafu(display("malformed group"))]
-    Group,
-
-    /// Illegal regex
-    #[snafu(display("invalid regex"))]
-    Regex { source: regex::Error },
-}
-
-fn fragments_from_string(s: &str) -> Result<Vec<Fragment>, Error> {
-    let mut walker = StringWalker::new(s);
-    let mut builder = vec![];
-    let mut text = String::new();
-    while let Some(ch) = walker.next() {
-        let next_token = match ch {
-            '?' => Some(Fragment::MatchOne),
-            '*' if walker.peek() == Some('*') => {
-                walker.eat(1);
-                Some(Fragment::MatchAny)
-            }
-            '*' => Some(Fragment::MatchAnyExceptForwardSlash),
-            '\\' => Some(Fragment::BackSlash),
-            '/' => Some(Fragment::ForwardSlash),
-            '.' => Some(Fragment::Dot),
-            '(' => {
-                let Some(end) = walker.substring_to(')') else {
-                    return Err(Error::Group);
-                };
-
-                walker.eat(end.len() + 1);
-
-                let splits = end.split(':').collect::<Vec<_>>();
-                if splits.len() != 2 {
-                    return Err(Error::Group);
-                }
-                let key = *splits.first().ok_or(Error::Group)?;
-                let value = *splits.get(1).ok_or(Error::Group)?;
-
-                let subpattern = fragments_from_string(value)?;
-                builder.push(Fragment::Group(key.to_owned(), subpattern));
-
-                None
-            }
-            ')' => None,
-            _ => {
-                text.push(ch);
-                None
-            }
-        };
-
-        if let Some(token) = next_token {
-            if !text.is_empty() {
-                builder.push(Fragment::Text(text.clone()));
-                text.clear();
-            }
-            builder.push(token);
-        }
-    }
-
-    if !text.is_empty() {
-        builder.push(Fragment::Text(text.clone()));
-    }
-
-    Ok(builder)
-}
-
-fn fragment_to_regex_str(fragment: &Fragment) -> (String, Vec<String>) {
-    let mut groups = vec![];
-    let string = match fragment {
-        Fragment::MatchOne => ".".into(),
-        Fragment::MatchAnyExceptForwardSlash => "[^\\/]*".into(),
-        Fragment::MatchAny => ".*".into(),
-        Fragment::BackSlash => "\\".into(),
-        Fragment::ForwardSlash => "\\/".into(),
-        Fragment::Dot => "\\.".into(),
-        Fragment::Text(t) => t.clone(),
-        Fragment::Group(id, elements) => {
-            let elements = elements
-                .iter()
-                .map(|m| {
-                    let (s, g) = fragment_to_regex_str(m);
-                    groups.extend(g);
-                    s
-                })
-                .collect::<String>();
-            groups.push(id.clone());
-            format!("(?<{id}>{elements})")
-        }
-    };
-    (string, groups)
-}
-
-impl FromStr for Pattern {
-    type Err = Error;
-
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let fragments = fragments_from_string(s)?;
-        let mut groups = BTreeSet::new();
-
-        let compiled = fragments
-            .iter()
-            .map(|m| {
-                let (s, g) = fragment_to_regex_str(m);
-                groups.extend(g);
-                s
-            })
-            .collect::<String>();
-
-        Ok(Self {
-            pattern: s.into(),
-            regex: Regex::new(&format!("^{compiled}$")).context(RegexSnafu)?,
-            groups: groups.into_iter().collect(),
-        })
-    }
-}
-
-impl<'de> Deserialize<'de> for Pattern {
+impl<'de> de::Deserialize<'de> for Pattern {
     fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
     where
         D: serde_core::Deserializer<'de>,
     {
-        let s = String::deserialize(deserializer)?;
-        FromStr::from_str(&s).map_err(de::Error::custom)
+        Ok(String::deserialize(deserializer)?.into())
     }
 }
 
 impl PartialEq for Pattern {
     fn eq(&self, other: &Self) -> bool {
-        self.pattern == other.pattern && self.groups == other.groups
+        self.pattern == other.pattern
     }
 }
 
@@ -293,39 +81,195 @@ impl PartialOrd for Pattern {
 
 impl Ord for Pattern {
     fn cmp(&self, other: &Self) -> std::cmp::Ordering {
-        match self.pattern.cmp(&other.pattern) {
-            std::cmp::Ordering::Less => return std::cmp::Ordering::Less,
-            std::cmp::Ordering::Equal => {}
-            std::cmp::Ordering::Greater => return std::cmp::Ordering::Greater,
-        }
-        self.groups.cmp(&other.groups)
+        self.pattern.cmp(&other.pattern)
     }
 }
 
-#[cfg(test)]
-pub mod path_tests {
-    use super::Pattern;
-
-    /// test me
-    #[test]
-    fn test_pattern() {
-        let k = "/usr/lib/modules/(version:*)/*".parse::<Pattern>().unwrap();
-
-        let good = k.match_path("/usr/lib/modules/6.2.6/modules.symbols");
-        assert!(good.is_some());
-        let m = good.unwrap();
-        assert_eq!(m.path, "/usr/lib/modules/6.2.6/modules.symbols");
-        let version = m.variables.get("version");
-        assert!(version.is_some());
-        assert_eq!(version.unwrap(), "6.2.6");
-
-        let wide = k.match_path("/usr/lib/modules/6.6.67-51.kvm/kernel/net/netfilter/nft_hash.ko.zst");
-        assert!(wide.is_none());
+impl Pattern {
+    /// Creates a new Pattern.
+    pub fn new(s: impl Into<String>) -> Self {
+        s.into().into()
     }
 
-    #[test]
-    fn test_match_any_regex() {
-        let pattern = "/usr/share/fonts/**/*.ttf".parse::<Pattern>().unwrap();
-        assert_eq!(pattern.regex.as_str(), r#"^\/usr\/share\/fonts\/.*\/[^\/]*\.ttf$"#);
+    /// Tries to match the pattern against a given file path.
+    /// If it matches, it returns the HashMap with the results of named wildcards.
+    /// If no named wildcard was specified in the pattern string, the HashMap is empty.
+    /// If the pattern doesn't match the path, None is returned.
+    pub fn matches(&self, path: impl AsRef<str>) -> Option<HashMap<String, String>> {
+        let mut path = path.as_ref();
+        let mut matches = HashMap::new();
+
+        let (head, tail, body) = head_tail_body(&self.tokens);
+        if !self.match_head(&mut path, head, &mut matches) {
+            return None;
+        }
+        if !self.match_tail(&mut path, tail.rev(), &mut matches) {
+            return None;
+        }
+        if !self.match_body(&mut path, body, &mut matches) {
+            return None;
+        }
+        Some(matches)
     }
+
+    fn match_head<'a, I>(&self, walker: &mut &str, head: I, matches: &mut HashMap<String, String>) -> bool
+    where
+        I: Iterator<Item = &'a Token>,
+    {
+        for tok in head {
+            match tok {
+                Token::Text(txt) => {
+                    if !walker.starts_with(&self.pattern[txt.clone()]) {
+                        return false;
+                    }
+                    *walker = &walker[txt.len()..];
+                }
+
+                Token::Wildcard {
+                    name,
+                    matcher: Matcher::One,
+                } => {
+                    let Some(next_char) = walker.chars().next() else {
+                        return false;
+                    };
+                    if next_char == MAIN_SEPARATOR {
+                        return false;
+                    }
+                    self.save_group_match(name, matches, next_char);
+                    *walker = &walker[next_char.len_utf8()..];
+                }
+
+                _ => unreachable!(),
+            }
+        }
+        true
+    }
+
+    fn match_tail<'a, I>(&self, walker: &mut &str, head: I, matches: &mut HashMap<String, String>) -> bool
+    where
+        I: Iterator<Item = &'a Token>,
+    {
+        for tok in head {
+            match tok {
+                Token::Text(txt) => {
+                    if !walker.ends_with(&self.pattern[txt.clone()]) {
+                        return false;
+                    }
+                    *walker = &walker[..walker.len() - txt.len()];
+                }
+
+                Token::Wildcard {
+                    name,
+                    matcher: Matcher::One,
+                } => {
+                    let Some(next_char) = walker.chars().next_back() else {
+                        return false;
+                    };
+                    if next_char == MAIN_SEPARATOR {
+                        return false;
+                    }
+                    self.save_group_match(name, matches, next_char);
+                    *walker = &walker[..walker.len() - next_char.len_utf8()];
+                }
+
+                _ => unreachable!(),
+            }
+        }
+        true
+    }
+
+    fn match_body<'a, I>(&self, walker: &mut &str, body: I, matches: &mut HashMap<String, String>) -> bool
+    where
+        I: Iterator<Item = &'a Token>,
+    {
+        let mut glob_group_name = None;
+        for tok in body {
+            match tok {
+                Token::Text(txt) => {
+                    let Some(index) = walker.find(&self.pattern[txt.clone()]) else {
+                        return false;
+                    };
+                    self.save_group_match(&glob_group_name, matches, &walker[..index]);
+                    *walker = &walker[index + txt.len()..];
+                }
+
+                Token::Wildcard {
+                    name,
+                    matcher: Matcher::One,
+                } => {
+                    let Some(next_char) = walker.chars().next() else {
+                        return false;
+                    };
+                    if next_char == MAIN_SEPARATOR {
+                        return false;
+                    }
+                    self.save_group_match(name, matches, next_char);
+                    *walker = &walker[next_char.len_utf8()..];
+                }
+
+                Token::Wildcard {
+                    name,
+                    matcher: Matcher::Any,
+                    ..
+                } => {
+                    // In the body, an any-glob is basically a separator.
+                    // As a side effect, consecutive any-globs are ignored
+                    // and only the last one is considered.
+                    glob_group_name = name.clone();
+                }
+            }
+        }
+        if *walker == "/" {
+            return false;
+        }
+        self.save_group_match(&glob_group_name, matches, *walker);
+        true
+    }
+
+    fn save_group_match(
+        &self,
+        name: &Option<Range<usize>>,
+        matches: &mut HashMap<String, String>,
+        value: impl Into<String>,
+    ) {
+        if let Some(group_name) = name {
+            matches.insert(self.pattern[group_name.clone()].to_string(), value.into());
+        }
+    }
+}
+
+fn head_tail_body(
+    tokens: &[Token],
+) -> (
+    impl Iterator<Item = &Token>,
+    impl DoubleEndedIterator<Item = &Token>,
+    impl Iterator<Item = &Token>,
+) {
+    fn is_glob(tok: &Token) -> bool {
+        matches!(
+            tok,
+            Token::Wildcard {
+                matcher: Matcher::Any,
+                ..
+            }
+        )
+    }
+
+    let head = Range {
+        start: 0,
+        end: tokens.iter().position(is_glob).map_or(tokens.len(), |i| i.min(1)),
+    };
+    let tail = Range {
+        start: tokens[head.end..]
+            .iter()
+            .rposition(is_glob)
+            .map_or(tokens.len(), |i| head.end + i + 1),
+        end: tokens.len(),
+    };
+    let body = Range {
+        start: head.end,
+        end: tail.start,
+    };
+
+    (tokens[head].iter(), tokens[tail].iter(), tokens[body].iter())
 }
